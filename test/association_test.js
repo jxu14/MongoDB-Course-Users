f6039b119c58e65b1a4336286725340c46b158e4
jxu14 <jeremyxu123@gmail.com>
2018-07-24 18:25:47 -0700

    associated user, blogPost, and comment instance to each other in association_test file

--- test/association_test.js
+++ test/association_test.js
@@ -1,8 +1,22 @@
 const mongoose = require('mongoose');
-const User = require('./src/user');
-const Comment = require('./src/comment');
-const BlogPost = require('./src/blogPost');
+const User = require('../src/user');
+const Comment = require('../src/comment');
+const BlogPost = require('../src/blogPost');
 
 describe('Associations', () => {
-  
+  //make sure all three variables are lowercase
+  //the capital BlogPost and Comment (above) are
+  //the model classes, which lowercase is just
+  //and instance of the class
+  let joe, blogPost, comment;
+  beforeEach((done) =>
+    joe = new User({ name: 'Joe' });
+    blogPost = new BlogPost({ title: 'JS is Great', content: 'Yep it really is' });
+    comment = new Comment({ content: 'Congrats on each comment' });
+
+    //ObjectId type refers to this blogPost in the user schema
+    joe.blogPosts.push(blogPost);
+    blogPost.comments.push(comment);
+    comment.user = joe;
+  });
 });
